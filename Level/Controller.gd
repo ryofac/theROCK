ce35971259d193da987b462e95aa48733c2fdfd5
ryofac <ryanfaustinocarvalho@gmail.com>
2023-07-11 17:29:37 -0300

    Updating the assets

--- Level/Controller.gd
+++ Level/Controller.gd
@@ -1,17 +1,14 @@
 extends Node
-var rockNode : Node
+
 export var playerScene : PackedScene
 
+onready var rockNode = get_parent().get_node("TheRock")
 onready var camera = get_parent().get_node("TheRock/Camera2D")
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
 func _spawn_player(_x, _y):
-	Global.players_spawned += 1
 	var player = playerScene.instance()
+	Global.players_spawned += 1	
 	player.add_to_group("player")
 	player.position.x = _x
 	player.position.y = _y
@@ -23,17 +20,24 @@ func _spawn_player(_x, _y):
 	
 func _process(delta):
 	var players = get_tree().get_nodes_in_group("player")
-	if Input.is_action_just_pressed("spawn_player"):
-		_spawn_player(rand_range(camera.global_position.x - 200, camera.global_position.x - 400), rand_range(camera.global_position.y - 100, camera.global_position.y - 200))
+	var player_count = len(players)
+	
+	if player_count <= 50:
+		if Input.is_action_just_pressed("spawn_player"):
+			# Spawn player
+			_spawn_player(rand_range(camera.global_position.x - 200, camera.global_position.x - 400), rand_range(camera.global_position.y - 100, camera.global_position.y - 200))
+	else:
+		print("Não pode haver mais de 50 players na tela!")	
+	get_parent().get_node("TheRock/Label").text += '\n Player count = ' + str(player_count) if player_count <= 50 else "\nNão pode haver mais que 50 jogadores!"
+		
 	if Input.is_action_just_pressed("delete_player"):
-		print(players)
 		delete_random_player(players)
+
 
 
 func delete_random_player(player_list):
 	if len(player_list) > 0:
 		player_list[randi() % len(player_list)].queue_free()
 		
-
 	
 	
--- Level/MainLevel.gd
+++ Level/MainLevel.gd
@@ -1,11 +1,11 @@
 extends TileMap
 
-var UPDATE_RATE = 32
+var UPDATE_RATE = 30
 
 func _ready():
-	for i in range(32):
-		_create_tile(i, 20)
-
+	for i in range(1, 4):
+		for cell_x in range(0, 32):
+			_create_tile(cell_x, 20 + i)
 
 func _process(delta):
 	var posx = get_node("TheRock/Camera2D").global_position.x
@@ -18,12 +18,12 @@ func _on_Button_pressed():
 	_create_tile(int(rand_range(10, 20)), int(rand_range(10, 20)))
 
 
-func _create_tile(_x,_y):
-	set_cell(_x, _y, 0)
+func _create_tile(_x,_y, type=0):
+	set_cell(_x, _y, type)
 
 
-func _delete_tile(_x, _y):
-	set_cell(_x, _y, -1)
+func _delete_tile(_x, _y, type=0):
+	set_cell(_x, _y, type)
 
 
 func adjust_terrain():
@@ -35,12 +35,14 @@ func adjust_terrain():
 	var border = get_node("TheRock").velocity.x / 2
 	
 	# Delete BORDER tiles to the left:
-	for cell_x in range(0, cx - border):
-		_delete_tile(cell_x, 20)
+	for i in range(1, 4):
+		for cell_x in range(0, cx - border):
+			_delete_tile(cell_x, 20, 1) if i <= 0 else _delete_tile(cell_x, 20 + i, 0)
 
 	# Delete BORDER tiles to the right:
-	for cell_x in range(cx, cx + border):
-		_create_tile(cell_x, 20)
+	for i in range(1, 4):
+		for cell_x in range(cx, cx + border):
+			_create_tile(cell_x, 20, 1) if i <= 0 else _create_tile(cell_x, 20 + i, 0)
 		
 		
 	
