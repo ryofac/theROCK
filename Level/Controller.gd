c6758b4cfd722663a5cc2debe09173c759dc0da0
ryofac <ryanfaustinocarvalho@gmail.com>
2023-07-12 14:26:28 -0300

    Fixed Camera

--- Level/Controller.gd
+++ Level/Controller.gd
@@ -19,30 +19,38 @@ func _ready() -> void:
 	var _ws = websocket.instance()
 	self.add_child(_ws)
 	print("Websocket instanciado: " + str(_ws))
+	
 
 
-func _spawn_player(_x=camera.global_position.x - 500, _y=rand_range(camera.global_position.y - 100, camera.global_position.y - 200)):
+func _spawn_player(_x=null, _y=null):
+	if (_x == null):
+		_x = camera.global_position.x - 500 * camera.zoom.x
+	if (_y == null):
+		_y = camera.global_position.y - 240 * camera.zoom.y
 	var name_list = ['Patro', 'Hermínio', 'Henrique', 'Lívia', 'Meireles', 'Ryan']
-	var player_list = [finnNode, frogNode, ninjaNode, guyNode]
+	var player_list = [finnNode, frogNode, ninjaNode]
 	var player = player_list[randi() % len(player_list)].instance()
 	Global.players_spawned += 1	
 	player.add_to_group("player")
 	player.get_node("Label").text = name_list[randi() % len(name_list)]
 	player.position.x = _x
 	player.position.y = _y
+	var _spawnBorder = 48
+	player.global_position.x = camera.global_position.x - camera.zoom.x * 500 + _spawnBorder
 	player.id = Global.players_spawned
 	get_parent().add_child(player)
+	print("Player instanciado na posicao: " + str(_x) + " e " + str(_y))
+	return player
 	
 	
 	
 func _process(delta):
 	var players = get_tree().get_nodes_in_group("player")
 	var player_count = len(players)
-	print('executando')
 	
 	if player_count <= 50:
 		if Input.is_action_just_pressed("spawn_player"):
-			print('Tentando spwantar')
+			print('Tentando spwanar')
 			# Spawn player
 			_spawn_player()
 #	get_parent().get_node("TheRock/Label").text += '\n Player count = ' + str(player_count) if player_count <= 50 else "\nNão pode haver mais que 50 jogadores!"
@@ -59,17 +67,17 @@ func delete_random_player(player_list):
 		random_player.kill()
 		
 func adjust_zoom(player_list):
-	var factor_zoom = floor(len(player_list) / 5) * 0.1
-	var _newZoom = 1 + factor_zoom
+	var start_zoom = 0.8
+	var factor_zoom = floor(len(player_list) / 10) * 0.1
+	var _newZoom = start_zoom + factor_zoom
 	var _actualZoom = camera.zoom.x
 	var _diff = (_newZoom - _actualZoom)
 	var _vel = 0.025
 	if abs(_diff) > _vel: 
-		_actualZoom += _vel
+		_actualZoom += _vel * sign(_diff)
 	else:
 		_actualZoom = _newZoom
-	print(_actualZoom)
 	camera.zoom = Vector2(_actualZoom, _actualZoom)
-	camera.limit_bottom = 800 + 80 * _actualZoom
-
+	camera.global_position.y = 550 - _actualZoom * 100
+	camera.limit_bottom = start_zoom * 1000 + 80 * _actualZoom
 	
