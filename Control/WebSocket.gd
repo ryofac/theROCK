21ff7d546e6c58c7e1d2362f808d2afdbe7ca798
Ryan <ryanfaustinocarvalho@gmail.com>
2023-07-14 02:31:46 -0300

    Initial Changes for QrCode/2

--- Control/WebSocket.gd
+++ Control/WebSocket.gd
@@ -11,6 +11,10 @@ onready var qrcode = get_node_or_null("qrcode")
 onready var header = get_node_or_null("header")
 onready var debugLabel = get_node_or_null("debug")
 onready var controller = get_parent().get_node("Controller")
+var _err = 0
+var exeption_err_not_ok = false
+var exeption_occurred = false
+
 
 
 
@@ -31,31 +35,47 @@ func spawnPlayer(_x, _y):
 func _ready():
 	debugText("Iniciando conexão...")
 	
-	# Atribuir sprite do QR Code
-#	qrcode.texture = QRCodeSprite
-	
 	# Definir funções de callback para cada evento:
 	_client.connect("data_received", self, "data_received")
 	_client.connect("connection_established", self, "connection_established")
 	_client.connect("connection_closed", self, "connection_closed")
+	try_to_connect()
 	
-	
-	# Conferir eventuais erros, e interromper processo caso haja:
-	var _err = _client.connect_to_url(_url)
-	if _err != OK:
-		set_process(false)
-		debugText("Falha ao conectar.")
 		
 func _process(_delta):
 	# Receber eventos de network.
+	if is_offline():
+		if $Timer.is_stopped(): $Timer.start()
+	else:
+		$Timer.stop()
 	_client.poll()
+	
+
+	
+func try_to_connect():
+	var _err = _client.connect_to_url(_url)
+	print('Tentando conectar...')
+	print("CLIENT STATUS: " + str(_client.get_connection_status()))
+	if _err != OK :
+		set_process(false)
+		debugText("Falha Grave ao conectar (_err != OK)")
+		exeption_occurred = true
+	else:
+		exeption_occurred = false
+
+	if is_offline():
+		print("Agora estou oficialmente offline!")
+		$Timer.start()
+		
+	elif is_connecting():
+		$Timer2.start()
 
 func connection_established(_subProtocol):
 	debugText("Conexão estabelecida!")
 	
 func connection_closed(_clean):
 	debugText("Conexão encerrada.")
-	if _clean: debugText("De maneira limpa.")
+	if _clean: debugText("De maneira limpa. (Possível memória cheia)")
 	
 	
 func data_received():
@@ -86,12 +106,11 @@ func send(msg):
 func is_online():
 	return _client.get_connection_status() == 2
 	
-func is_conneting():
+func is_connecting():
 	return _client.get_connection_status() == 1
 
 func is_offline():
-	return _client.get_connection_status() == 0
-
+	return _client.get_connection_status() == 0 or exeption_occurred
 
 
 
@@ -104,4 +123,16 @@ func is_offline():
 #
 #	# Reiniciar jogo
 #	if get_tree().reload_current_scene() == OK:
-#		print("Reiniciando jogo.")
+#		print("Reiniciando jogo."
+
+
+func _on_Timer2_timeout():
+	if not is_online():
+		print('Timeout')
+		try_to_connect()
+		if is_offline():
+			if $Timer.is_stopped(): $Timer.start()
+
+
+func _on_Timer_timeout():
+	print("Estou offline")
